05e6ce5b7f5eaf556369c33699d7e3d1c5d8e0e5
Marcin Lewandowski <marcin.lewandowski996@gmail.com>
2017-10-29 00:40:02 +0200

    Configured webpack, babel, typescript

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { render } from "react-dom";
+import App from "./components/App";
+import * as React from "react";
+
+render(
+    <App />,
+    document.getElementById("app-root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const extractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: {
+        'app': './app/index'
+    },
+    output: {
+        path: path.join(__dirname, 'www', 'dist'),
+        publicPath: '/dist/',
+        filename: '[name]-bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, include: /app/, exclude: /node_modules/, loaders: ['babel-loader'] },
+            { test: /(\.css)$/, loader: extractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
+            { test: /\.jsx$/, include: /app/, loaders: ['babel-loader'] },
+            { test: /\.ts$/, include: /app/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react!ts-loader' },
+            { test: /\.tsx$/, include: /app/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react!ts-loader' }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.web.ts',
+            '.ts',
+            '.web.tsx',
+            '.tsx',
+            '.web.js',
+            '.js',
+            '.json',
+            '.web.jsx',
+            '.jsx',
+        ],
+    },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new extractTextPlugin('[name].css'),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ]
+};
