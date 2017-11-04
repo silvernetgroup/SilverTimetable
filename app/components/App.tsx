f6b35df6116515f07731204faf58ebf90f7b20ea
Marcin Lewandowski <marcin.lewandowski996@gmail.com>
2017-11-04 20:51:57 +0100

    fix

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import TimeTableServices from "..\\..\\TimetableServices";
+import * as React from "react";
+import * as TimeTableServices from "..\\..\\TimetableServices";
 import FlatButton from "material-ui/FlatButton";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import TexField from "material-ui/TextField";
+import TextField from "material-ui/TextField";
 
 export default class App extends React.Component {
     render(): JSX.Element {
         return (
-            //<h1>Silver Timetable</h1>
+            // <h1>Silver Timetable</h1>
             <MuiThemeProvider>
                 <div>
-                    <TexField id="text" value={} />
+                    <TextField id="text" value="DUPSKO" />
                 </div>
             </MuiThemeProvider>
         );
