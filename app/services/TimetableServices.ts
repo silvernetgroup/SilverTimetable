7f89673d618f7155ee4862f7db1aa59645dc82d7
Tomasz Kondraciuk <tomek.kondraciuk@op.pl>
2017-11-07 19:13:13 +0100

    Compilable Version

--- app/services/TimetableServices.ts
+++ app/services/TimetableServices.ts
@@ -3,9 +3,15 @@ export class TimeTableServices {
 
     public static downloadTimetableFile(url: string): string {
 
-        var xhttp: XMLHttpRequest = new XMLHttpRequest();
-        xhttp.open("GET", url, true);
-        xhttp.send(null);
-        return xhttp.responseText;
+        var result: string=""
+        var xhttp = new XMLHttpRequest();
+       xhttp.onreadystatechange=function(){
+           if(this.readyState==4 && this.status==200){
+               result=xhttp.responseText
+           }
+       }
+        xhttp.open("GET",url,true)
+        xhttp.send()
+        return "Ready state: "+xhttp.readyState+"\nStatus: "+xhttp.status
     }
 }
