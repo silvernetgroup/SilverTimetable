fcae601af49c8bf5a5524216f40f2fb5e06815ff
Marcin Lewandowski <marcin.lewandowski996@gmail.com>
2017-11-18 19:08:57 +0100

    Configured firebase messages. Azure hub however doesn't work.

--- www/js/push.js
+++ www/js/push.js
@@ -1,37 +1,48 @@
-const push = PushNotification.init({
-	android: {
-	},
-    browser: {
-        pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-    },
-	ios: {
-		alert: "true",
-		badge: "true",
-		sound: "true"
-	},
-	windows: {}
-});
+window.onload = this.onLoad;
+console.log("plik")
+function onLoad() {
+	document.addEventListener("deviceready", onDeviceReady, false);
+	console.log("onLoad");
+}
 
-push.on('registration', (data) => {
-	console.log("regID: "+data.registrationId);
-});
+function onDeviceReady() {
+	console.log("ready");
+	const push = PushNotification.init({
+		android: {
+			senderID: "948434874310"
+		},
+		browser: {
+			pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+		},
+		ios: {
+			alert: "true",
+			badge: "true",
+			sound: "true"
+		},
+		windows: {}
+	});
 
-push.on('notification', (data) => {
-	// data.message,
-	// data.title,
-	// data.count,
-	// data.sound,
-	// data.image,
-    // data.additionalData
-    console.log('notification event');
-    navigator.notification.alert(
-        data.message,         // message
-        null,                 // callback
-        data.title,           // title
-        'Ok'                  // buttonName
-    );
-});
+	push.on('registration', (data) => {
+		console.log("regID: " + data.registrationId);
+	});
 
-push.on('error', (e) => {
-	// e.message
-});
+	push.on('notification', (data) => {
+		// data.message,
+		// data.title,
+		// data.count,
+		// data.sound,
+		// data.image,
+		// data.additionalData
+		console.log('notification event');
+		navigator.notification.alert(
+			data.message,         // message
+			null,                 // callback
+			data.title,           // title
+			'Ok'                  // buttonName
+		);
+	});
+
+	push.on('error', (e) => {
+		// e.message
+	});	
+}
