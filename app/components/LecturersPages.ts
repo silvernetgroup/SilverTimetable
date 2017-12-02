8f0cab41f5a487d466de8bc78b166496ecf6deab
Marcin Lewandowski <marcin.lewandowski996@gmail.com>
2017-12-02 17:24:33 +0100

    Plugin reinstall, refactor, added missing chars replacements

--- app/components/LecturersPages.ts
+++ app/components/LecturersPages.ts
@@ -6,21 +6,23 @@ declare let cordova: any;
 
 export default class LecturersPages {
 
-    public static openLecturersPage(event: ITimetableEvent ) {
-        let name: string = event.lecturer.toLowerCase().replace(" ", "_").replace("ą", "a").replace("ć", "c");
-        name = name.replace("ę", "e").replace("ł", "l").replace("ó", "o").replace("ż", "z").replace("ź", "z");
+    public static openLecturersPage(event: ITimetableEvent) {
+        const name: string = event.lecturer.toLowerCase()
+            .replace(" ", "_")
+            .replace("ą", "a")
+            .replace("ć", "c")
+            .replace("ę", "e")
+            .replace("ł", "l")
+            .replace("ó", "o")
+            .replace("ż", "z")
+            .replace("ź", "z")
+            .replace("ś", "s")
+            .replace("ń", "n");
         const linkURL: string = this.getLecturerPage(name);
-        window.open = cordova.InAppBrowser.open(linkURL, "_system", "location=yes");
+        cordova.InAppBrowser.open(linkURL, "_system", "location=yes");
     }
 
-    private static getLecturerPage(lecturer: string): string {
-        if (LecturersPages[lecturer] !== undefined) {
-            return LecturersPages[lecturer];
-        }
-        return "http://www.wzim.sggw.pl/" + lecturer + "/";
-    }
-
-    private lecturersPages: any = {
+    private static lecturersPages: any = {
         "krzysztof_gajowniczek": "http://krzysztof_gajowniczek.users.sggw.pl/",
         "ewa_jalowiecka": "http://www.sggw.pl/o_pracowniku&employee_id=1175692",
         "piotr_jalowiecki": "http://www.sggw.pl/o_pracowniku&employee_id=1175694",
@@ -38,4 +40,11 @@ export default class LecturersPages {
         "monika_zielinska-sitkiewicz": "http://www.sggw.pl/o_pracowniku&employee_id=1621855",
         "wojciech_zielisnki": "http://wojtek.zielinski.statystyka.info",
     };
+
+    private static getLecturerPage(lecturer: string): string {
+        if (LecturersPages[lecturer] !== undefined) {
+            return this.lecturersPages[lecturer];
+        }
+        return "http://www.wzim.sggw.pl/" + lecturer + "/";
+    }
 }
