6bdf5046f8891fb7c64a1533ade88a117443e81f
Marcin Lewandowski <marcin.lewandowski996@gmail.com>
2017-12-08 22:05:56 +0100

    Finally it works

--- www/js/push.js
+++ www/js/push.js
@@ -20,8 +20,35 @@ function onDeviceReady() {
 		windows: {}
 	});
 
+	// declare var WindowsAzure: any;
+	var client = new WindowsAzure.MobileServiceClient("https://silvertimetable.azurewebsites.net");
+
 	push.on('registration', (data) => {
 		console.log("regID: " + data.registrationId);
+
+		// Get the native platform of the device.
+		var platform = device.platform;
+		// Get the handle returned during registration.
+		var handle = data.registrationId;
+		// Set the device-specific message template.
+		if (platform == 'android' || platform == 'Android') {
+			// Register for GCM notifications.
+			client.push.register('gcm', handle, {
+				mytemplate: { body: { data: { message: "{$(messageParam)}" } } }
+			});
+		} else if (device.platform === 'iOS') {
+			// Register for notifications.
+			client.push.register('apns', handle, {
+				mytemplate: { body: { aps: { alert: "{$(messageParam)}" } } }
+			});
+		} else if (device.platform === 'windows') {
+			// Register for WNS notifications.
+			client.push.register('wns', handle, {
+				myTemplate: {
+					body: '<toast><visual><binding template="ToastText01"><text id="1">$(messageParam)</text></binding></visual></toast>',
+					headers: { 'X-WNS-Type': 'wns/toast' } }
+			});
+		}
 	});
 
 	push.on('notification', (data) => {
