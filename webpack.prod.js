2786574653c5b5e3c96117fbafc419763afff3da
Silver .NET Group <kontakt@silver.sggw.pl>
2018-01-14 02:35:15 +0100

    production webpack configuration (#62)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,11 @@
 const webpack = require('webpack');
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const extractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    devtool: 'inline-source-map',
+    devtool: 'cheap-module-source-map',
     entry: {
         'app': './app/index'
     },
@@ -40,7 +41,10 @@ module.exports = {
         new webpack.NoEmitOnErrorsPlugin(),
         new extractTextPlugin('[name].css'),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        })
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new UglifyJSPlugin({
+              sourceMap: true
+            }),
     ]
 };
